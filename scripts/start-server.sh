451b35f8c3199708bd0c8789b16d1b29a529e128
LouiIII3 <jikdsunghuilee@naver.com>
2024-11-08 01:12:40 +0900

    [setting/#9]: jar 파일 이름 변경

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # JAR 파일 위치로 이동
-cd /home/ubuntu/woori-zip-BE
+cd /home/ubuntu/woori-zip-BE/build/libs
 
 # Spring Boot 애플리케이션 시작
-nohup java -jar woori-zip-BE.jar > /home/ubuntu/woori-zip-BE/logs/app.log 2>&1 &
+nohup java -jar backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/woori-zip-BE/logs/app.log 2>&1 &
