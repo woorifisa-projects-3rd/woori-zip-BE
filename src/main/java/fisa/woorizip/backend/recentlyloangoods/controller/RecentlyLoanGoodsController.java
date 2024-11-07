55d7aa8e170818f1a433c5595807bb2c71ac1390
qbobl5 <ddddd991125@gmail.com>
2024-11-07 16:58:12 +0900

    [setting]: recently_loan_goods 패키지 설정

---
+++ src/main/java/fisa/woorizip/backend/recentlyloangoods/controller/RecentlyLoanGoodsController.java
@@ -0,0 +1,13 @@
+package fisa.woorizip.backend.recentlyloangoods.controller;
+
+import fisa.woorizip.backend.recentlyloangoods.service.RecentlyLoanGoodsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class RecentlyLoanGoodsController {
+    private final RecentlyLoanGoodsService recentlyLoanGoodsService;
+}
