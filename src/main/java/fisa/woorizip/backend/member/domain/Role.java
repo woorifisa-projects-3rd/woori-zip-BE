04d8f5094848d1de3f03ae3ae8e0015078bb44a6
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 15:51:45 +0900

    [feat/#12]: 접근 가능한 권한인지 확인 하는 기능

--- src/main/java/fisa/woorizip/backend/member/domain/Role.java
+++ src/main/java/fisa/woorizip/backend/member/domain/Role.java
@@ -2,15 +2,23 @@ package fisa.woorizip.backend.member.domain;
 
 import lombok.Getter;
 
+import java.util.Set;
+
 @Getter
 public enum Role {
-    MEMBER("회원"),
-    AGENT("공인중개사"),
-    ADMIN("관리자");
+    MEMBER("회원", Set.of()),
+    AGENT("공인중개사", Set.of(MEMBER)),
+    ADMIN("관리자", Set.of(MEMBER, AGENT));
 
     private final String name;
+    private final Set<Role> suRoles;
 
-    Role(String name) {
+    Role(String name, Set<Role> suRoles) {
         this.name = name;
+        this.suRoles = suRoles;
+    }
+
+    public boolean canAccess(Role requiredRole) {
+        return requiredRole == this || this.getSuRoles().contains(requiredRole);
     }
 }
