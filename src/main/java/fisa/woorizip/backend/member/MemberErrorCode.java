04c958bc90e25e8d6cf3c90ee2c4aa1a70012904
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 12:40:54 +0900

    [feat/#4]: 소득 유형 ErrorCode 추가
    - 소득 유형의 type이 맞지 않을 때

--- src/main/java/fisa/woorizip/backend/member/MemberErrorCode.java
+++ src/main/java/fisa/woorizip/backend/member/MemberErrorCode.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 public enum MemberErrorCode implements ErrorCode {
-    ;
+    EARNINGS_TYPE_NOT_FOUND(NOT_FOUND, "소득 유형이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
