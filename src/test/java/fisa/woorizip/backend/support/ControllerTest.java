b304f18ac0105c106e29c733592c553c41a9afbf
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 15:51:56 +0900

    [feat/#12]: 컨트롤러 테스트에 인증 부분 추가

--- src/test/java/fisa/woorizip/backend/support/ControllerTest.java
+++ src/test/java/fisa/woorizip/backend/support/ControllerTest.java
@@ -1,21 +1,36 @@
 package fisa.woorizip.backend.support;
 
+import fisa.woorizip.backend.member.domain.Member;
+import fisa.woorizip.backend.member.service.auth.JwtTokenProvider;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
 
     @LocalServerPort private int port;
 
+    @Autowired private JwtTokenProvider jwtTokenProvider;
+
     @BeforeEach
     void setPort() {
         RestAssured.port = port;
+    }
+
+    public RequestSpecification baseRestAssuredWithAuth(Member member) {
+        return RestAssured.given()
+                .header(AUTHORIZATION, "Bearer " + jwtTokenProvider.createAccessToken(member))
+                .log()
+                .all()
+                .contentType(ContentType.JSON);
     }
 
     public RequestSpecification baseRestAssured() {
