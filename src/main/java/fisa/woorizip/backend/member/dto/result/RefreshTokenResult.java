edddc6c0006b323bc9de5cbabae5e97e20043943
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 17:19:16 +0900

    [feat/#12]: 로그인 result 추가
    - signInResponse, RefreshTokenResult

---
+++ src/main/java/fisa/woorizip/backend/member/dto/result/RefreshTokenResult.java
@@ -0,0 +1,25 @@
+package fisa.woorizip.backend.member.dto.result;
+
+import fisa.woorizip.backend.member.domain.RefreshToken;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class RefreshTokenResult {
+
+    private String value;
+    private Long expirationSeconds;
+
+    @Builder
+    public RefreshTokenResult(String value, Long expirationSeconds) {
+        this.value = value;
+        this.expirationSeconds = expirationSeconds;
+    }
+
+    public static RefreshTokenResult of(RefreshToken refreshToken, Long expirationSeconds) {
+        return RefreshTokenResult.builder()
+                .value(refreshToken.getValue())
+                .expirationSeconds(expirationSeconds)
+                .build();
+    }
+}
