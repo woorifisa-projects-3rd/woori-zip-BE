48822819cfc889d220077373b8878bf2380759f2
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 15:49:21 +0900

    [feat/#12]: 쿠키 생성기 추가
    - 리프레시 토큰 쿠키
    - 로그아웃 쿠키

---
+++ src/main/java/fisa/woorizip/backend/member/controller/auth/CookieProvider.java
@@ -0,0 +1,33 @@
+package fisa.woorizip.backend.member.controller.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CookieProvider {
+
+    public static final String REFRESH_TOKEN = "refresh_token";
+
+    @Value("${security.refresh.expiration}")
+    private Long expirationSeconds;
+
+    public ResponseCookie createRefreshTokenCookie(String refreshToken) {
+        return createBaseRefreshTokenCookie(refreshToken).maxAge(expirationSeconds).build();
+    }
+
+    private ResponseCookie.ResponseCookieBuilder createBaseRefreshTokenCookie(String cookieValue) {
+        return ResponseCookie.from(REFRESH_TOKEN, cookieValue)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .sameSite(Cookie.SameSite.NONE.attributeValue());
+    }
+
+    public ResponseCookie createSignOutCookie() {
+        return createBaseRefreshTokenCookie("").maxAge(0).build();
+    }
+}
