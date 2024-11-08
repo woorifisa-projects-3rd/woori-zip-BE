b782329203497c7d9ca7c1b80bad6b3c2bbafe15
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-08 15:48:06 +0900

    [feat/#12]: Login 어노테이션 추가
    - 로그인 필수 여부 받기 위함

---
+++ src/main/java/fisa/woorizip/backend/member/controller/auth/Login.java
@@ -0,0 +1,16 @@
+package fisa.woorizip.backend.member.controller.auth;
+
+import fisa.woorizip.backend.member.domain.Role;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Login {
+
+    Role role() default Role.MEMBER;
+
+}
