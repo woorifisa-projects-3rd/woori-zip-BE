0861448a9944a4dd83a71c80e6c7835af28e7a4c
LouiIII3 <jikdsunghuilee@naver.com>
2024-11-08 16:26:11 +0900

    [setting/#9]: 포트번호 변경

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -9,11 +9,11 @@ cd /home/ubuntu/woori-zip-BE/build/libs
 
 # Spring Boot 애플리케이션 시작 (환경 변수에 따라 포트 설정)
 if [ "$BRANCH_NAME" == "release/v**" ]; then
-    nohup java -Dserver.port=${SOCKET_PORT_TEST} \
+    nohup java -Dserver.port=${SOCKET_PORT_PROD} \
     -jar backend-0.0.1-SNAPSHOT.jar \
     --spring.config.location=file:/home/ubuntu/config/application.yml > /home/ubuntu/woori-zip-BE/logs/app-prod.log 2>&1 &
 else
-    nohup java -Dserver.port=${SOCKET_PORT_PROD} \
+    nohup java -Dserver.port=${SOCKET_PORT_TEST} \
     -jar backend-0.0.1-SNAPSHOT.jar \
     --spring.config.location=file:/home/ubuntu/config/application.yml > /home/ubuntu/woori-zip-BE/logs/app-test.log 2>&1 &
 fi
