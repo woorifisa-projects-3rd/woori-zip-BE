71c5d18ee115bcc396d8c2f5bb3f0612043831fd
qbobl5 <ddddd991125@gmail.com>
2024-11-09 14:48:28 +0900

    [feat/#5]: facility 에 error code 추가 - CATEGORY_NOT_FOUND 존재하지 않는 카테고리를 요청받은 경우

--- src/main/java/fisa/woorizip/backend/facility/FacilityErrorCode.java
+++ src/main/java/fisa/woorizip/backend/facility/FacilityErrorCode.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 public enum FacilityErrorCode implements ErrorCode {
-    ;
+    CATEGORY_NOT_FOUND(NOT_FOUND, "카테고리가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
