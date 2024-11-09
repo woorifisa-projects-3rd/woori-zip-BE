22fad89dc43305d1c3811e2285b2fe6c991196b9
qbobl5 <ddddd991125@gmail.com>
2024-11-09 14:47:39 +0900

    [feat/#5]: category 수정 - NONE(없음) 경우 추가 - 예외 처리

--- src/main/java/fisa/woorizip/backend/facility/domain/Category.java
+++ src/main/java/fisa/woorizip/backend/facility/domain/Category.java
@@ -1,14 +1,27 @@
 package fisa.woorizip.backend.facility.domain;
 
+import fisa.woorizip.backend.common.exception.WooriZipException;
 import lombok.Getter;
+
+import java.util.Arrays;
+
+import static fisa.woorizip.backend.facility.FacilityErrorCode.CATEGORY_NOT_FOUND;
 
 @Getter
 public enum Category {
-    FOOD("요식업");
+    FOOD("요식업"),
+    NONE("없음");
 
     private final String name;
 
     Category(String name) {
         this.name = name;
+    }
+
+    public static Category from(String name) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.getName().equals(name))
+                .findAny()
+                .orElseThrow(() -> new WooriZipException(CATEGORY_NOT_FOUND));
     }
 }
