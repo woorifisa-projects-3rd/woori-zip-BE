c2857713ae6bde3d1ac67058bf7f6cc688ebcd8a
qbobl5 <ddddd991125@gmail.com>
2024-11-09 14:54:23 +0900

    [feat/#5]: 지도에서 필터링을 요청한 값 -> MapFilterRequest 실제로 프론트에서 요청 시 RequestParam 과 동일 - 0 이상인 min~ 값들 @PositiveOrZero 로 유효성 검사 - 0 보다 커야하는 max~ 값들 @Positive 로 유효성 검사 - 위도 및 경도 @Digits 로 형식 유효성 검사     - 정수 부분 3 자리 이하, 소수 부분 6 자리 이하 - enum 타입은 String/int 로 받고 from 으로 생성하여 넣어주기 - param 에 전달되지 않을 수 있는 값들 Wrapper 로 받고 null 확인 후 초기화

---
+++ src/main/java/fisa/woorizip/backend/house/dto/MapFilterRequest.java
@@ -0,0 +1,104 @@
+package fisa.woorizip.backend.house.dto;
+
+import fisa.woorizip.backend.facility.domain.Category;
+import fisa.woorizip.backend.house.domain.HouseType;
+import fisa.woorizip.backend.house.domain.HousingExpenses;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+
+import java.beans.ConstructorProperties;
+import java.math.BigDecimal;
+
+@Getter
+public class MapFilterRequest {
+    private MapLevel level;
+
+    @Digits(integer = 3, fraction = 6)
+    private BigDecimal southWestLatitude;
+
+    @Digits(integer = 3, fraction = 6)
+    private BigDecimal southWestLongitude;
+
+    @Digits(integer = 3, fraction = 6)
+    private BigDecimal northEastLatitude;
+
+    @Digits(integer = 3, fraction = 6)
+    private BigDecimal northEastLongitude;
+
+    private HouseType houseType;
+    private HousingExpenses housingExpenses;
+    @PositiveOrZero private long minDeposit;
+    @Positive private long maxDeposit;
+    @PositiveOrZero private long minMonthlyRentFee;
+    @Positive
+    private long maxMonthlyRentFee;
+    @PositiveOrZero
+    private long minMaintenanceFee;
+    @Positive private long maxMaintenanceFee;
+    private Category category;
+    private int walking;
+    private int facilityCount;
+    private String gu;
+    private String dong;
+
+    @ConstructorProperties({
+        "level",
+        "southWestLatitude",
+        "southWestLongitude",
+        "northEastLatitude",
+        "northEastLongitude",
+        "houseType",
+        "housingExpenses",
+        "minDeposit",
+        "maxDeposit",
+        "minMonthlyRentFee",
+        "maxMonthlyRentFee",
+        "minMaintenanceFee",
+        "maxMaintenanceFee",
+        "category",
+        "walking",
+        "facilityCount",
+        "gu",
+        "dong"
+    })
+    private MapFilterRequest(
+            int level,
+            BigDecimal southWestLatitude,
+            BigDecimal southWestLongitude,
+            BigDecimal northEastLatitude,
+            BigDecimal northEastLongitude,
+            String houseType,
+            String housingExpenses,
+            long minDeposit,
+            long maxDeposit,
+            long minMonthlyRentFee,
+            long maxMonthlyRentFee,
+            long minMaintenanceFee,
+            long maxMaintenanceFee,
+            String category,
+            Integer walking,
+            Integer facilityCount,
+            String gu,
+            String dong) {
+        this.level = MapLevel.from(level);
+        this.southWestLatitude = southWestLatitude;
+        this.southWestLongitude = southWestLongitude;
+        this.northEastLatitude = northEastLatitude;
+        this.northEastLongitude = northEastLongitude;
+        this.houseType = HouseType.from(houseType);
+        this.housingExpenses = HousingExpenses.from(housingExpenses);
+        this.minDeposit = minDeposit;
+        this.maxDeposit = maxDeposit;
+        this.minMonthlyRentFee = minMonthlyRentFee;
+        this.maxMonthlyRentFee = maxMonthlyRentFee;
+        this.minMaintenanceFee = minMaintenanceFee;
+        this.maxMaintenanceFee = maxMaintenanceFee;
+        this.category = Category.from(category);
+        this.walking = walking == null ? 0 : walking;
+        this.facilityCount = facilityCount == null ? 0 : facilityCount;
+        this.gu = gu == null ? "" : gu;
+        this.dong = dong == null ? "" : dong;
+    }
+}
