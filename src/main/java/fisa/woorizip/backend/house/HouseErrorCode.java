8bafe8ded434aafed8fc62d2b92867146321fc52
qbobl5 <ddddd991125@gmail.com>
2024-11-09 14:55:57 +0900

    [feat/#5]: error code 추가

--- src/main/java/fisa/woorizip/backend/house/HouseErrorCode.java
+++ src/main/java/fisa/woorizip/backend/house/HouseErrorCode.java
@@ -3,12 +3,16 @@ package fisa.woorizip.backend.house;
 import fisa.woorizip.backend.common.exception.errorcode.ErrorCode;
 
 import lombok.Getter;
-
 import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 public enum HouseErrorCode implements ErrorCode {
-    ;
+    HOUSE_ADDRESS_TYPE_NOT_FOUND(NOT_FOUND, "집 주소 형식이 존재하지 않습니다."),
+    MAP_LEVEL_NOT_FOUND(NOT_FOUND, "줌 레벨이 존재하지 않습니다."),
+    HOUSE_TYPE_NOT_FOUND(NOT_FOUND, "집 종류가 존재하지 않습니다."),
+    HOUSING_EXPENSES_NOT_FOUND(NOT_FOUND, "주거 비용 타입이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
