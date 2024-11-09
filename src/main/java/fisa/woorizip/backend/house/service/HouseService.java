9b76db9e87ebfca21518230cdb2edaf340c42132
qbobl5 <ddddd991125@gmail.com>
2024-11-09 15:00:22 +0900

    [feat/#5]: 지도를 조회하는 서비스 메인 메소드 형식 구현

--- src/main/java/fisa/woorizip/backend/house/service/HouseService.java
+++ src/main/java/fisa/woorizip/backend/house/service/HouseService.java
@@ -1,3 +1,16 @@
 package fisa.woorizip.backend.house.service;
 
-public interface HouseService {}
+import fisa.woorizip.backend.facility.domain.Category;
+import fisa.woorizip.backend.house.domain.HouseType;
+import fisa.woorizip.backend.house.domain.HousingExpenses;
+import fisa.woorizip.backend.house.dto.MapFilterRequest;
+import fisa.woorizip.backend.house.dto.MapLevel;
+import fisa.woorizip.backend.house.dto.ShowMapResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface HouseService {
+
+    List<ShowMapResponse> showMap(MapFilterRequest mapFilterRequest);
+}
--- src/main/java/fisa/woorizip/backend/house/service/HouseServiceImpl.java
+++ src/main/java/fisa/woorizip/backend/house/service/HouseServiceImpl.java
@@ -1,5 +1,10 @@
 package fisa.woorizip.backend.house.service;
 
+import fisa.woorizip.backend.facility.domain.Category;
+import fisa.woorizip.backend.house.domain.HouseType;
+import fisa.woorizip.backend.house.domain.HousingExpenses;
+import fisa.woorizip.backend.house.dto.MapFilterRequest;
+import fisa.woorizip.backend.house.dto.ShowMapResponse;
 import fisa.woorizip.backend.house.repository.HouseRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -7,10 +12,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HouseServiceImpl implements HouseService {
 
     private final HouseRepository houseRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ShowMapResponse> showMap(MapFilterRequest mapFilterRequest) {
+
+        return null;
+    }
 }
