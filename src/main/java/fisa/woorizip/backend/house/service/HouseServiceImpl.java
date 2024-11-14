0814413ad4e054f68adf549ac4e039a5eb3d9900
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-14 17:19:23 +0900

    [chore/#10]: HouseServiceImpl 동작 추가
    - 하우스데이터가져오기 및 예외처리

--- src/main/java/fisa/woorizip/backend/house/service/HouseServiceImpl.java
+++ src/main/java/fisa/woorizip/backend/house/service/HouseServiceImpl.java
@@ -1,16 +1,36 @@
 package fisa.woorizip.backend.house.service;
 
+import fisa.woorizip.backend.common.exception.WooriZipException;
+import fisa.woorizip.backend.house.domain.House;
+import fisa.woorizip.backend.house.dto.response.HouseDetailResponse;
 import fisa.woorizip.backend.house.repository.HouseRepository;
-
+import fisa.woorizip.backend.houseimage.repository.HouseImageRepository;
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static fisa.woorizip.backend.house.HouseErrorCode.HOUSE_NOT_FOUND;
+
+@Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HouseServiceImpl implements HouseService {
-
     private final HouseRepository houseRepository;
+    private final HouseImageRepository houseImageRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public HouseDetailResponse getHouseDetail(Long houseId) {
+        House house = findHouseById(houseId);
+        List<String> imageUrls = houseImageRepository.findImageUrlsByHouseId(houseId);
+        return HouseDetailResponse.of(house, imageUrls);
+    }
+
+    private House findHouseById(Long houseId) {
+        return houseRepository.findById(houseId)
+                .orElseThrow(() -> new WooriZipException(HOUSE_NOT_FOUND));
+    }
 }
