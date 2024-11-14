db3f8f5ab3439f0709f6c0a06b95a4fcf363a46c
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-14 17:14:17 +0900

    [chore/#10]: GetMapping 추가
    - 디테일 리스펀스 가져오는 맵핑 추가

--- src/main/java/fisa/woorizip/backend/house/controller/HouseController.java
+++ src/main/java/fisa/woorizip/backend/house/controller/HouseController.java
@@ -1,16 +1,21 @@
 package fisa.woorizip.backend.house.controller;
 
+import fisa.woorizip.backend.house.dto.response.HouseDetailResponse;
 import fisa.woorizip.backend.house.service.HouseService;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/houses")
 public class HouseController {
-
     private final HouseService houseService;
+
+    @GetMapping("/{houseId}")
+    public HouseDetailResponse getHouseDetail(@PathVariable("houseId") Long houseId) {
+        log.info("houseId {}", houseId);
+        return houseService.getHouseDetail(houseId);
+    }
 }
