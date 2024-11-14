e3878725f1111dfb310c9862c571ead0347e9ba9
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-14 17:31:37 +0900

    [chore/#10]: HouseErrorCode 예외처리
    - 예외 상태 확인 및 예외처리

--- src/main/java/fisa/woorizip/backend/house/HouseErrorCode.java
+++ src/main/java/fisa/woorizip/backend/house/HouseErrorCode.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 public enum HouseErrorCode implements ErrorCode {
-    ;
+
+    HOUSE_NOT_FOUND(NOT_FOUND, "매물을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
@@ -17,4 +20,6 @@ public enum HouseErrorCode implements ErrorCode {
         this.status = status;
         this.message = message;
     }
+
+
 }
