50d4a68f2f13d6ee6d67a2325c3006e7494bae21
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-15 10:33:44 +0900

    [fix/#10]: House ImageFixture 추가

---
+++ src/test/java/fisa/woorizip/backend/support/fixture/HouseImageFixture.java
@@ -0,0 +1,33 @@
+package fisa.woorizip.backend.support.fixture;
+
+import fisa.woorizip.backend.house.domain.House;
+import fisa.woorizip.backend.houseimage.domain.HouseImage;
+
+public class HouseImageFixture {
+
+    private Long id;
+    private String url = "https://example.com/sample-image.jpg";
+    private int orderIndex = 0;
+
+    public static HouseImageFixture builder() {
+        return new HouseImageFixture();
+    }
+
+    public HouseImageFixture url(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public HouseImageFixture orderIndex(int orderIndex) {
+        this.orderIndex = orderIndex;
+        return this;
+    }
+
+    public HouseImage build(House house) {
+        return HouseImage.builder()
+                .house(house)
+                .url(url)
+                .orderIndex(orderIndex)
+                .build();
+    }
+}
