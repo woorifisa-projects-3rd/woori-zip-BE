9b733f4dc4cbe8bf3be1e5b55f5c9db671552927
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-17 14:19:54 +0900

    [feat/#10]: HouseImageErrorCode 에러코드정의가추

--- src/main/java/fisa/woorizip/backend/houseimage/HouseImageErrorCode.java
+++ src/main/java/fisa/woorizip/backend/houseimage/HouseImageErrorCode.java
@@ -6,9 +6,16 @@ import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 public enum HouseImageErrorCode implements ErrorCode {
-    ;
+    IMAGE_NOT_FOUND(NOT_FOUND, "이미지가 존재하지 않습니다."),
+    INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식입니다."),
+    IMAGE_SIZE_EXCEEDED(HttpStatus.PAYLOAD_TOO_LARGE, "이미지 크기가 제한을 초과했습니다."),
+    MAX_IMAGE_COUNT_EXCEEDED(HttpStatus.BAD_REQUEST, "이미지 개수가 제한을 초과했습니다."),
+    INVALID_ORDER_INDEX(HttpStatus.BAD_REQUEST, "잘못된 이미지 순서 값입니다."),
+    DUPLICATE_ORDER_INDEX(HttpStatus.CONFLICT, "중복된 이미지 순서 값이 존재합니다.");
 
     private final HttpStatus status;
     private final String message;
