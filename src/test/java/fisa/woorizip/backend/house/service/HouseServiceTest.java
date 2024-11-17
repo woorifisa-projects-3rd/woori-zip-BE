60f98da42c1298b429b1e41bf24c083637e91b29
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-17 15:24:04 +0900

    [feat/#10]: HouseServiceTest 변수명 명확하게 수정

--- src/test/java/fisa/woorizip/backend/house/service/HouseServiceTest.java
+++ src/test/java/fisa/woorizip/backend/house/service/HouseServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.BDDMockito.*;
 
 import fisa.woorizip.backend.common.exception.WooriZipException;
 import fisa.woorizip.backend.house.domain.House;
+import fisa.woorizip.backend.house.dto.response.HouseDetailResponse;
 import fisa.woorizip.backend.house.repository.HouseRepository;
 import fisa.woorizip.backend.houseimage.repository.HouseImageRepository;
 import fisa.woorizip.backend.support.fixture.HouseFixture;
@@ -30,30 +31,25 @@ class HouseServiceTest {
 
     @Test
     void 매물_상세정보와_이미지URL_조회_성공() {
-        // given
-        House house = HouseFixture.builder().build();
-        List<String> imageUrls = List.of("test1.jpg", "test2.jpg");
+        House testHouse = HouseFixture.builder().build();
+        List<String> testImageUrls = List.of("test1.jpg", "test2.jpg");
 
-        given(houseRepository.findById(anyLong())).willReturn(Optional.of(house));
-        given(houseImageRepository.findImageUrlsByHouseId(anyLong())).willReturn(imageUrls);
+        given(houseRepository.findById(1L)).willReturn(Optional.of(testHouse));
+        given(houseImageRepository.findImageUrlsByHouseId(1L)).willReturn(testImageUrls);
 
-        // when
-        var response = houseService.getHouseDetail(1L);
+        HouseDetailResponse houseDetailResponse = houseService.getHouseDetail(1L);
 
-        // then
-        assertThat(response)
-                .satisfies(r -> {
-                    assertThat(r.getName()).isEqualTo(house.getName());
-                    assertThat(r.getImageUrls()).containsExactlyElementsOf(imageUrls);
+        assertThat(houseDetailResponse)
+                .satisfies(response -> {
+                    assertThat(response.getName()).isEqualTo(testHouse.getName());
+                    assertThat(response.getImageUrls()).containsExactlyElementsOf(testImageUrls);
                 });
     }
 
     @Test
     void 존재하지_않는_매물_조회시_예외발생() {
-        // given
-        given(houseRepository.findById(anyLong())).willReturn(Optional.empty());
+        given(houseRepository.findById(999L)).willReturn(Optional.empty());
 
-        // when & then
         assertThatThrownBy(() -> houseService.getHouseDetail(999L))
                 .isInstanceOf(WooriZipException.class);
     }
