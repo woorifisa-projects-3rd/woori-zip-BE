5977ef929d5408129730f200f10b7847c4a30eb3
qbobl5 <ddddd991125@gmail.com>
2024-11-18 12:43:45 +0900

    [feat/#5]: test 를 위한 생성자 추가

--- src/main/java/fisa/woorizip/backend/facility/dto/FacilityResult.java
+++ src/main/java/fisa/woorizip/backend/facility/dto/FacilityResult.java
@@ -7,11 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FacilityResult {
     private Long facilityId;
     private double latitude;
     private double longitude;
+
+    public static FacilityResult init(Facility facility) {
+        return new FacilityResult(
+                facility.getId(), facility.getLatitude(), facility.getLongitude());
+    }
 }
--- src/main/java/fisa/woorizip/backend/house/dto/result/HouseResult.java
+++ src/main/java/fisa/woorizip/backend/house/dto/result/HouseResult.java
@@ -1,6 +1,7 @@
 package fisa.woorizip.backend.house.dto.result;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import fisa.woorizip.backend.facility.domain.Facility;
 import fisa.woorizip.backend.facility.dto.FacilityResult;
 import fisa.woorizip.backend.house.domain.House;
 import lombok.AllArgsConstructor;
@@ -22,5 +23,13 @@ public class HouseResult {
 
     public static HouseResult init(House house) {
         return new HouseResult(house.getId(), house.getLatitude(), house.getLongitude(), null);
+    }
+
+    public static HouseResult init(House house, List<Facility> facilities) {
+        return new HouseResult(
+                house.getId(),
+                house.getLatitude(),
+                house.getLongitude(),
+                facilities.stream().map(FacilityResult::init).toList());
     }
 }
