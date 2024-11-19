203be4b1ede883e1237b63d6ee4f466c80246cf5
qbobl5 <ddddd991125@gmail.com>
2024-11-19 21:29:13 +0900

    [refactor/#26]: dto 코드 리팩토링

--- src/main/java/fisa/woorizip/backend/house/dto/request/MapFilterRequest.java
+++ src/main/java/fisa/woorizip/backend/house/dto/request/MapFilterRequest.java
@@ -3,6 +3,7 @@ package fisa.woorizip.backend.house.dto.request;
 import static fisa.woorizip.backend.facility.domain.Category.NONE;
 import static fisa.woorizip.backend.house.domain.HouseType.ALL;
 import static fisa.woorizip.backend.house.domain.HousingExpenses.ANY;
+import static java.util.Objects.isNull;
 
 import fisa.woorizip.backend.facility.domain.Category;
 import fisa.woorizip.backend.house.domain.HouseType;
@@ -19,49 +20,49 @@ import java.beans.ConstructorProperties;
 
 @Getter
 public class MapFilterRequest {
-    private MapLevel level;
+    private final MapLevel level;
 
-    @NotNull private double southWestLatitude;
+    @NotNull final private double southWestLatitude;
 
-    @NotNull private double southWestLongitude;
+    @NotNull final private double southWestLongitude;
 
-    @NotNull private double northEastLatitude;
+    @NotNull final private double northEastLatitude;
 
-    @NotNull private double northEastLongitude;
+    @NotNull final private double northEastLongitude;
 
-    private HouseType houseType;
-    private HousingExpenses housingExpenses;
-    @PositiveOrZero private long minDeposit;
-    @Positive private long maxDeposit;
-    @PositiveOrZero private long minMonthlyRentFee;
-    @Positive private long maxMonthlyRentFee;
-    @PositiveOrZero private long minMaintenanceFee;
-    @Positive private long maxMaintenanceFee;
-    private Category category;
-    private Integer walking;
-    private Integer facilityCount;
-    private String gu;
-    private String dong;
+    private final HouseType houseType;
+    private final HousingExpenses housingExpenses;
+    @PositiveOrZero final private long minDeposit;
+    @Positive final private long maxDeposit;
+    @PositiveOrZero final private long minMonthlyRentFee;
+    @Positive final private long maxMonthlyRentFee;
+    @PositiveOrZero final private long minMaintenanceFee;
+    @Positive final private long maxMaintenanceFee;
+    private final Category category;
+    private final Integer walking;
+    private final Integer facilityCount;
+    private final String gu;
+    private final String dong;
 
     @ConstructorProperties({
-        "level",
-        "southWestLatitude",
-        "southWestLongitude",
-        "northEastLatitude",
-        "northEastLongitude",
-        "houseType",
-        "housingExpenses",
-        "minDeposit",
-        "maxDeposit",
-        "minMonthlyRentFee",
-        "maxMonthlyRentFee",
-        "minMaintenanceFee",
-        "maxMaintenanceFee",
-        "category",
-        "walking",
-        "facilityCount",
-        "gu",
-        "dong"
+            "level",
+            "southWestLatitude",
+            "southWestLongitude",
+            "northEastLatitude",
+            "northEastLongitude",
+            "houseType",
+            "housingExpenses",
+            "minDeposit",
+            "maxDeposit",
+            "minMonthlyRentFee",
+            "maxMonthlyRentFee",
+            "minMaintenanceFee",
+            "maxMaintenanceFee",
+            "category",
+            "walking",
+            "facilityCount",
+            "gu",
+            "dong"
     })
     private MapFilterRequest(
             int level,
@@ -87,16 +88,16 @@ public class MapFilterRequest {
         this.southWestLongitude = southWestLongitude;
         this.northEastLatitude = northEastLatitude;
         this.northEastLongitude = northEastLongitude;
-        this.houseType = houseType == null ? ALL : HouseType.from(houseType);
+        this.houseType = isNull(houseType) ? ALL : HouseType.from(houseType);
         this.housingExpenses =
-                housingExpenses == null ? ANY : HousingExpenses.from(housingExpenses);
-        this.minDeposit = minDeposit == null ? 0 : minDeposit;
-        this.maxDeposit = maxDeposit == null ? Long.MAX_VALUE : maxDeposit;
-        this.minMonthlyRentFee = minMonthlyRentFee == null ? 0 : minMonthlyRentFee;
-        this.maxMonthlyRentFee = maxMonthlyRentFee == null ? Long.MAX_VALUE : maxMonthlyRentFee;
-        this.minMaintenanceFee = minMaintenanceFee == null ? 0 : minMaintenanceFee;
-        this.maxMaintenanceFee = maxMaintenanceFee == null ? Long.MAX_VALUE : maxMaintenanceFee;
-        this.category = category == null ? NONE : Category.from(category);
+                isNull(housingExpenses) ? ANY : HousingExpenses.from(housingExpenses);
+        this.minDeposit = isNull(minDeposit) ? 0 : minDeposit;
+        this.maxDeposit = isNull(maxDeposit) ? Long.MAX_VALUE : maxDeposit;
+        this.minMonthlyRentFee = isNull(minMonthlyRentFee) ? 0 : minMonthlyRentFee;
+        this.maxMonthlyRentFee = isNull(maxMonthlyRentFee) ? Long.MAX_VALUE : maxMonthlyRentFee;
+        this.minMaintenanceFee = isNull(minMaintenanceFee) ? 0 : minMaintenanceFee;
+        this.maxMaintenanceFee = isNull(maxMaintenanceFee) ? Long.MAX_VALUE : maxMaintenanceFee;
+        this.category = isNull(category) ? NONE : Category.from(category);
         this.walking = walking;
         this.facilityCount = facilityCount;
         this.gu = gu;
