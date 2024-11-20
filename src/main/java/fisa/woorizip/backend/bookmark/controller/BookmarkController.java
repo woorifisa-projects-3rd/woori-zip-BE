0a0f4e1b7f541e90ab1a930a54eb13815d703a29
Jieun-KWAK <jieun47631@gmail.com>
2024-11-20 14:25:41 +0900

    [feat/#28] : 북마크 목록 추가 컨트롤러 작성

--- src/main/java/fisa/woorizip/backend/bookmark/controller/BookmarkController.java
+++ src/main/java/fisa/woorizip/backend/bookmark/controller/BookmarkController.java
@@ -1,25 +1,26 @@
 package fisa.woorizip.backend.bookmark.controller;
 
-import fisa.woorizip.backend.bookmark.domain.Bookmark;
-import fisa.woorizip.backend.bookmark.dto.request.BookmarkRequest;
 import fisa.woorizip.backend.bookmark.service.BookmarkService;
 
+import fisa.woorizip.backend.member.controller.auth.Login;
+import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
+import fisa.woorizip.backend.member.controller.auth.VerifiedMember;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/houses/{houseId}")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
+    @Login
     @PostMapping("/bookmark")
-    public ResponseEntity<Void> addBookmark(@RequestBody BookmarkRequest request) {
-        bookmarkService.addBookmark(request);
+    public ResponseEntity<Void> addBookmark(@VerifiedMember MemberIdentity memberIdentity, @PathVariable Long houseId) {
+        bookmarkService.addBookmark(memberIdentity, houseId);
         return ResponseEntity.ok().build();
     }
 
