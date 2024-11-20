ab2052393639fdc3ee87fccc56b6756f6c5d3080
Jieun-KWAK <jieun47631@gmail.com>
2024-11-20 14:29:04 +0900

    [feat/#28] : 북마크 목록 추가 서비스 작성

--- src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
+++ src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
@@ -2,7 +2,9 @@ package fisa.woorizip.backend.bookmark.service;
 
 import fisa.woorizip.backend.bookmark.domain.Bookmark;
 import fisa.woorizip.backend.bookmark.dto.request.BookmarkRequest;
+import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
 
 public interface BookmarkService {
-    void addBookmark(BookmarkRequest bookmarkRequest);
+
+    void addBookmark(MemberIdentity memberIdentity, Long houseId);
 }
