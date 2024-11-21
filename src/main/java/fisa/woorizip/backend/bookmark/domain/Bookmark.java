30d927e2b884b7128653b055f10d8e7e471ac511
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:13:11 +0900

    [feat/#33]: Bookmark 수정
    - 생성자 내부에서 createdAt 필드를 LocalDateTime.now()로 초기화하도록 수정
    - 엔티티 생성 시 별도로 createdAt을 설정하지 않아도 자동으로 현재 시간을 저장

--- src/main/java/fisa/woorizip/backend/bookmark/domain/Bookmark.java
+++ src/main/java/fisa/woorizip/backend/bookmark/domain/Bookmark.java
@@ -2,26 +2,17 @@ package fisa.woorizip.backend.bookmark.domain;
 
 import fisa.woorizip.backend.house.domain.House;
 import fisa.woorizip.backend.member.domain.Member;
-
 import jakarta.persistence.*;
-
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bookmark {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,6 +27,7 @@ public class Bookmark {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
 
     @Builder
     private Bookmark(Long id, Member member, House house, LocalDateTime createdAt) {
