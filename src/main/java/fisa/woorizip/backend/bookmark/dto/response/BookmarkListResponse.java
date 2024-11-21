f646e689ecb12e426702e83883393eb8d7edd8a4
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:13:59 +0900

    [feat/#33]: BookmarkListResponse 추가
    -

---
+++ src/main/java/fisa/woorizip/backend/bookmark/dto/response/BookmarkListResponse.java
@@ -0,0 +1,40 @@
+package fisa.woorizip.backend.bookmark.dto.response;
+
+import fisa.woorizip.backend.bookmark.domain.Bookmark;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class BookmarkListResponse {
+    private final Long bookmarkId;
+    private final Long houseId;
+    private final String houseName;
+    private final String address;
+    private final String gu;
+    private final String dong;
+    private final long deposit;
+    private final long monthlyRentFee;
+    private final String imageUrl;
+    private final LocalDateTime createdAt;
+
+    public static List<BookmarkListResponse> from(List<Bookmark> bookmarks) {
+        return bookmarks.stream()
+                .map(bookmark -> builder()
+                        .bookmarkId(bookmark.getId())
+                        .houseId(bookmark.getHouse().getId())
+                        .houseName(bookmark.getHouse().getName())
+                        .address(bookmark.getHouse().getAddress())
+                        .gu(bookmark.getHouse().getGu())
+                        .dong(bookmark.getHouse().getDong())
+                        .deposit(bookmark.getHouse().getDeposit())
+                        .monthlyRentFee(bookmark.getHouse().getMonthlyRentFee())
+                        .imageUrl(bookmark.getHouse().getRepresentativeImage())
+                        .createdAt(bookmark.getCreatedAt())
+                        .build())
+                .toList();
+    }
+}
