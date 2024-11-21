a81b0477f78d0590edbcc06c7ec8ffb922f4b199
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:17:46 +0900

    [feat/#33]: BookmarkServiceImpl 구현
    - 회원 ID와 페이징 정보를 기반으로 북마크 데이터를 조회
    - BookmarkRepository의 findBookmarksWithHouse 메서드 호출
    - 조회된 데이터를 BookmarkSliceResponse로 변환하여 반환

--- src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkServiceImpl.java
@@ -1,16 +1,23 @@
 package fisa.woorizip.backend.bookmark.service;
 
+import fisa.woorizip.backend.bookmark.domain.Bookmark;
+import fisa.woorizip.backend.bookmark.dto.response.BookmarkSliceResponse;
 import fisa.woorizip.backend.bookmark.repository.BookmarkRepository;
-
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookmarkServiceImpl implements BookmarkService {
+    private final BookmarkRepository bookmarkRepository; // 추가
 
-    private final BookmarkRepository bookmarkRepository;
+    @Override
+    @Transactional(readOnly = true)
+    public BookmarkSliceResponse getBookmarkList(Long memberId, Pageable pageable) {
+        Slice<Bookmark> bookmarkSlice = bookmarkRepository.findBookmarksWithHouse(memberId, pageable);
+        return BookmarkSliceResponse.from(bookmarkSlice);
+    }
 }
