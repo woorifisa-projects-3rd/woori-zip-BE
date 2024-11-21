fcf2aa509b59456c18c0eb65d6559395649396b6
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:16:59 +0900

    [feat/#33]: BookmarkService 구현

--- src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
+++ src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
@@ -1,3 +1,8 @@
 package fisa.woorizip.backend.bookmark.service;
 
-public interface BookmarkService {}
+import fisa.woorizip.backend.bookmark.dto.response.BookmarkSliceResponse;
+import org.springframework.data.domain.Pageable;
+
+public interface BookmarkService {
+    BookmarkSliceResponse getBookmarkList(Long memberId, Pageable pageable);
+}
