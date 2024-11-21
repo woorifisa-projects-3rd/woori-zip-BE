de24d5e1cd4cd7f09d6b904307dad8094fa64f8f
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:16:43 +0900

    [feat/#33]: BookmarkRepository 구현
    - BookmarkRepository 인터페이스의 불필요한 중복 선언 제거
    - 기존 @Repository 어노테이션 삭제, JpaRepository 기본 설정으로 처리

--- src/main/java/fisa/woorizip/backend/bookmark/repository/BookmarkRepository.java
+++ src/main/java/fisa/woorizip/backend/bookmark/repository/BookmarkRepository.java
@@ -1,7 +1,21 @@
 package fisa.woorizip.backend.bookmark.repository;
 
 import fisa.woorizip.backend.bookmark.domain.Bookmark;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {}
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    @Query(value = "SELECT b FROM Bookmark b " +
+            "JOIN FETCH b.house h " +
+            "WHERE b.member.id = :memberId " +
+            "ORDER BY b.createdAt DESC",
+            countQuery = "SELECT COUNT(b) FROM Bookmark b WHERE b.member.id = :memberId")
+    Slice<Bookmark> findBookmarksWithHouse(
+            @Param("memberId") Long memberId,
+            Pageable pageable);
+}
