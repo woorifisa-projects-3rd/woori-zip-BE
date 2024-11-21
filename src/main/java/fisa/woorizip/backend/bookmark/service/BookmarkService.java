40caf306c33d5548e4de7e85f37441cf763de61a
Jieun-KWAK <jieun47631@gmail.com>
2024-11-21 09:37:50 +0900

    [feat/#28] : 북마크 추가 api BookmarkService - getBookmarksByMember 추가

--- src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
+++ src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
@@ -1,10 +1,14 @@
 package fisa.woorizip.backend.bookmark.service;
 
 import fisa.woorizip.backend.bookmark.domain.Bookmark;
-import fisa.woorizip.backend.bookmark.dto.request.BookmarkRequest;
 import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
+
+import java.util.List;
 
 public interface BookmarkService {
 
     void addBookmark(MemberIdentity memberIdentity, Long houseId);
+
+    List<Bookmark> getBookmarksByMember(Long memberId);
+
 }
