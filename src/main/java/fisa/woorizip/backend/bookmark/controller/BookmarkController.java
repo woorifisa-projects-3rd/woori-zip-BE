fa2aa88f037c9bf0eb799c64ae34c620eb16365f
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-21 20:09:12 +0900

    [feat/#33]: BookmarkController 구현
    - /bookmarks 엔드포인트 추가
    - 회원 ID(`memberId`)를 기반으로 북마크 데이터를 페이징 처리하여 조회 가능
    - Pageable을 활용해 기본 페이지 크기(size)는 5로 설정
    - BookmarkService를 호출하여 북마크 데이터를 처리 및 반환

--- src/main/java/fisa/woorizip/backend/bookmark/controller/BookmarkController.java
+++ src/main/java/fisa/woorizip/backend/bookmark/controller/BookmarkController.java
@@ -1,16 +1,26 @@
+
 package fisa.woorizip.backend.bookmark.controller;
 
+import fisa.woorizip.backend.bookmark.dto.response.BookmarkSliceResponse;
 import fisa.woorizip.backend.bookmark.service.BookmarkService;
-
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class BookmarkController {
-
     private final BookmarkService bookmarkService;
+
+    @GetMapping("/bookmarks")
+    public BookmarkSliceResponse getBookmarkList(
+            @RequestParam("memberId") Long memberId,
+            @PageableDefault(size = 5) Pageable pageable) {
+        return bookmarkService.getBookmarkList(memberId, pageable);
+    }
 }
