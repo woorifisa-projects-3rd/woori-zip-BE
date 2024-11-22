06f0a5d998200303c9214d345344f7e4d30cf0cd
min20ta <kimminjis2206@gmail.com>
2024-11-22 10:31:11 +0900

    [fix/#32]: LoanGoodscustomRepository 삭제후 LoanGoodsRepository에 jpql추가

--- src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
+++ src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
@@ -2,12 +2,29 @@ package fisa.woorizip.backend.loangoods.repository;
 
 import fisa.woorizip.backend.loangoods.domain.LoanGoods;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface LoanGoodsRepository
-        extends JpaRepository<LoanGoods, Long>, LoanGoodsRepositoryCustom {
+public interface LoanGoodsRepository extends JpaRepository<LoanGoods, Long>{
 
     Optional<LoanGoods> findLoanGoodsById(Long longGoodsId);
-}
+
+    @Query("SELECT l FROM LoanGoods l " +
+                 "WHERE l.maxAssets >= : assets " +
+                 "AND l.maxTotalIncomeLastYear >= : totalIncomeLastYear " +
+                 "AND l.maxYearsOfMarriage >= : yearsOfMarriage " +
+                 "AND l.minCreditScore <= : creditScore " +
+                 "AND l.minMonthsOfEmployment <= : monthsOfEmployment " +
+                 "AND l.maxAge >= : internationalAge ")
+    List<LoanGoods> findLoanGoodsByCustomCriteria(
+         long assets,
+         long totalIncomeLastYear,
+         int yearsOfMarriage,
+         int creditScore,
+         int monthsOfEmployment,
+         int internationalAge
+     );
+    }
