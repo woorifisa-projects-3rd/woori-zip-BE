1708685106fe5cc8e17a9f471be2b24e9b79d333
min20ta <kimminjis2206@gmail.com>
2024-11-22 11:29:13 +0900

    [fix/#32]: 대출추천 보여줄 때 금리 낮은 순으로 보기 위해 order by 추가

--- src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
+++ src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
@@ -4,6 +4,7 @@ import fisa.woorizip.backend.loangoods.domain.LoanGoods;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,18 +14,19 @@ public interface LoanGoodsRepository extends JpaRepository<LoanGoods, Long>{
     Optional<LoanGoods> findLoanGoodsById(Long longGoodsId);
 
     @Query("SELECT l FROM LoanGoods l " +
-                 "WHERE l.maxAssets >= : assets " +
-                 "AND l.maxTotalIncomeLastYear >= : totalIncomeLastYear " +
-                 "AND l.maxYearsOfMarriage >= : yearsOfMarriage " +
-                 "AND l.minCreditScore <= : creditScore " +
-                 "AND l.minMonthsOfEmployment <= : monthsOfEmployment " +
-                 "AND l.maxAge >= : internationalAge ")
+                 "WHERE l.maxAssets >= :assets " +
+                 "AND l.maxTotalIncomeLastYear >= :totalIncomeLastYear " +
+                 "AND l.maxYearsOfMarriage >= :yearsOfMarriage " +
+                 "AND l.minCreditScore <= :creditScore " +
+                 "AND l.minMonthsOfEmployment <= :monthsOfEmployment " +
+                 "AND l.maxAge >= :internationalAge " +
+                 "ORDER BY l.interestRate")
     List<LoanGoods> findLoanGoodsByMemberInformation(
-         long assets,
-         long totalIncomeLastYear,
-         int yearsOfMarriage,
-         int creditScore,
-         int monthsOfEmployment,
-         int internationalAge
+        @Param("assets") long assets,
+        @Param("totalIncomeLastYear") long totalIncomeLastYear,
+        @Param("yearsOfMarriage") int yearsOfMarriage,
+        @Param("creditScore") int creditScore,
+        @Param("monthsOfEmployment") int monthsOfEmployment,
+        @Param("internationalAge") int internationalAge
      );
     }
