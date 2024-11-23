8982fe5b9edba1c0d69fa9841dd800588e5246e2
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-24 00:16:03 +0900

    [refactor/#33]: BookmarkService token 수정

--- src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
+++ src/main/java/fisa/woorizip/backend/bookmark/service/BookmarkService.java
@@ -8,5 +8,5 @@ public interface BookmarkService {
 
     void addBookmark(MemberIdentity memberIdentity, Long houseId);
 
-    BookmarkSliceResponse getBookmarkList(Long memberId, Pageable pageable);
+    BookmarkSliceResponse getBookmarkList(MemberIdentity memberIdentity, Pageable pageable);
 }
