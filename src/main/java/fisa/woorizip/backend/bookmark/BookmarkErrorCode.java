841d84f2c94215a1ef27759f4e30fc28672ebaf7
Jieun-KWAK <jieun47631@gmail.com>
2024-11-25 16:46:06 +0900

    [feat/#45] : 북마크 에러 코드 추가

--- src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
+++ src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
@@ -12,7 +12,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum BookmarkErrorCode implements ErrorCode {
     BOOKMARK_ALREADY_EXIST(CONFLICT, "이미 존재하는 북마크입니다."),
-    BOOKMARK_NOT_FOUND(NOT_FOUND, "북마크를 찾을 수 없습니다.");
+    BOOKMARK_NOT_FOUND(NOT_FOUND, "북마크를 찾을 수 없습니다."),
+    BOOKMARK_MEMBER_NOT_FOUND(NOT_FOUND, "북마크의 회원을 찾을 수 없습니다."),
+    BOOKMARK_HOUSE_NOT_FOUND(NOT_FOUND, "북마크의 집을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
