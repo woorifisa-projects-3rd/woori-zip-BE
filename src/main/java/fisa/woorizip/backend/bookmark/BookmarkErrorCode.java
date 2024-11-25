6f32cdd83d7185147609bea1f5daa53d179a936c
Jieun-KWAK <jieun47631@gmail.com>
2024-11-25 12:35:31 +0900

    [feat/#45] : BookmarkFixture 작성

--- src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
+++ src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
@@ -1,6 +1,7 @@
 package fisa.woorizip.backend.bookmark;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import fisa.woorizip.backend.common.exception.errorcode.ErrorCode;
 
@@ -10,7 +11,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum BookmarkErrorCode implements ErrorCode {
-    BOOKMARK_ALREADY_EXIST(CONFLICT, "이미 존재하는 북마크입니다.");
+    BOOKMARK_ALREADY_EXIST(CONFLICT, "이미 존재하는 북마크입니다."),
+    BOOKMARK_NOT_FOUND(NOT_FOUND, "북마크를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
