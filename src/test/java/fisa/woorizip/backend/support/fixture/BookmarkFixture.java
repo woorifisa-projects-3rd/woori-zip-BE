e0d54aeffd8b9c8daefd91372c92647ca7b37ee3
Jieun-KWAK <jieun47631@gmail.com>
2024-11-25 12:46:07 +0900

    [feat/#45] : bookmarkFixture 작성

---
+++ src/test/java/fisa/woorizip/backend/support/fixture/BookmarkFixture.java
@@ -0,0 +1,51 @@
+package fisa.woorizip.backend.support.fixture;
+
+import fisa.woorizip.backend.bookmark.domain.Bookmark;
+import fisa.woorizip.backend.house.domain.House;
+import fisa.woorizip.backend.member.domain.Member;
+
+import java.time.LocalDateTime;
+
+public class BookmarkFixture {
+
+    private Long id;
+
+    private Member member;
+
+    private House house = House.builder().id((long) 1L).build();
+
+    private LocalDateTime createdAt = LocalDateTime.of(2000, 3, 31, 0, 0);
+
+    public static BookmarkFixture builder() {
+        return new BookmarkFixture();
+    }
+
+    public BookmarkFixture id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public BookmarkFixture member(Member member) {
+        this.member = member;
+        return this;
+    }
+
+    public BookmarkFixture house(House house) {
+        this.house = house;
+        return this;
+    }
+
+    public BookmarkFixture createdAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public Bookmark build() {
+        return Bookmark.builder()
+                .id(id)
+                .member(member)
+                .house(house)
+                .createdAt(createdAt)
+                .build();
+    }
+}
