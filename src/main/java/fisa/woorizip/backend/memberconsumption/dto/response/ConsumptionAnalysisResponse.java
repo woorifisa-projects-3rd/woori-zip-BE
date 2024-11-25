316552ee33cda4ed71e0e8051da34008b7848017
qbobl5 <ddddd991125@gmail.com>
2024-11-25 15:20:21 +0900

    [feat/#41]: dto 추가 - 가장 최적의 카테고리 response

--- src/main/java/fisa/woorizip/backend/memberconsumption/dto/response/ConsumptionAnalysisResponse.java
+++ src/main/java/fisa/woorizip/backend/memberconsumption/dto/response/ConsumptionAnalysisResponse.java
@@ -9,38 +9,26 @@ import java.util.List;
 
 @Getter
 public class ConsumptionAnalysisResponse {
-    private CustomerTypeResponse customerType;
-    private int customerTypeCount;
     private ConsumptionResponse memberConsumption;
     private ConsumptionResponse otherConsumption;
-    private List<Category> memberCategories;
-    private Category bestCategory;
+    private CategoryResponse bestCategory;
 
     private ConsumptionAnalysisResponse(
-            CustomerTypeResponse customerType,
-            int customerTypeCount,
             ConsumptionResponse memberConsumption,
             ConsumptionResponse otherConsumption,
-            List<Category> memberCategories,
-            Category bestCategory) {
-        this.customerType = customerType;
-        this.customerTypeCount = customerTypeCount;
+            CategoryResponse bestCategory) {
         this.memberConsumption = memberConsumption;
         this.otherConsumption = otherConsumption;
-        this.memberCategories = memberCategories;
         this.bestCategory = bestCategory;
     }
 
     public static ConsumptionAnalysisResponse of(
             ConsumptionAnalysis consumptionAnalysis,
             MemberConsumption memberConsumption,
-            List<String> categories) {
+            CategoryResponse bestCategory) {
         return new ConsumptionAnalysisResponse(
-                CustomerTypeResponse.from(memberConsumption.getMember()),
-                consumptionAnalysis.getCustomerCount(),
                 ConsumptionResponse.from(memberConsumption),
                 ConsumptionResponse.from(consumptionAnalysis),
-                categories.stream().map(Category::from).toList(),
-                Category.from(categories.get(0)));
+                bestCategory);
     }
 }
