f88ad9ad352640633bfa49a6aaa12c0ac5864ead
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-25 09:27:00 +0900

    [feat/#30]: oauth 코드 받아 로그인하는 기능 추가
    - 성공시 OK

--- src/main/java/fisa/woorizip/backend/member/controller/auth/AuthController.java
+++ src/main/java/fisa/woorizip/backend/member/controller/auth/AuthController.java
@@ -15,9 +15,11 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +29,11 @@ public class AuthController {
 
     private final AuthService authService;
     private final CookieProvider cookieProvider;
+
+    @GetMapping("/oauth")
+    public SignInResponse oauthLogin(@RequestParam String code) {
+        return authService.oauthLogin(code);
+    }
 
     @PostMapping("/sign-in")
     public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInRequest signInRequest) {
