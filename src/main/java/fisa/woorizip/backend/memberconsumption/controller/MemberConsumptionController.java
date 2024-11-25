c930b4255ce13055e6a087d345e9760d11f5e1ae
qbobl5 <ddddd991125@gmail.com>
2024-11-25 15:21:28 +0900

    [feat/#41]: 소비 패턴 분석 api 컨트롤러 구현

--- src/main/java/fisa/woorizip/backend/memberconsumption/controller/MemberConsumptionController.java
+++ src/main/java/fisa/woorizip/backend/memberconsumption/controller/MemberConsumptionController.java
@@ -1,7 +1,12 @@
 package fisa.woorizip.backend.memberconsumption.controller;
 
+import fisa.woorizip.backend.member.controller.auth.Login;
+import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
+import fisa.woorizip.backend.member.controller.auth.VerifiedMember;
+import fisa.woorizip.backend.memberconsumption.dto.response.ConsumptionAnalysisResponse;
 import fisa.woorizip.backend.memberconsumption.service.MemberConsumptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberConsumptionController {
 
     private final MemberConsumptionService memberConsumptionService;
+
+    @GetMapping
+    @Login
+    public ConsumptionAnalysisResponse showConsumptionAnalysis(@VerifiedMember MemberIdentity memberIdentity) {
+        return memberConsumptionService.getConsumptionAnalysis(memberIdentity);
+    }
 }
