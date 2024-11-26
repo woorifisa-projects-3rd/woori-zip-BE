641f19e956fb85ce5e8ca2ac390cf014c2959f32
Jieun-KWAK <jieun47631@gmail.com>
2024-11-26 11:58:39 +0900

    [feat/#45] : findByMemberIdAndHouseId 추가

--- src/main/java/fisa/woorizip/backend/bookmark/repository/BookmarkRepository.java
+++ src/main/java/fisa/woorizip/backend/bookmark/repository/BookmarkRepository.java
@@ -4,7 +4,11 @@ import fisa.woorizip.backend.bookmark.domain.Bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     boolean existsByMemberIdAndHouseId(Long memberId, Long houseId);
+
+    Optional <Bookmark> findByMemberIdAndHouseId(Long memberId, Long houseId);
 }
