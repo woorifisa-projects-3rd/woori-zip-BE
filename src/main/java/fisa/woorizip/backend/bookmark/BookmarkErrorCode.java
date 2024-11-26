fb3d99d5250c8e768099249f4845c2771d3e5f35
Jieun-KWAK <jieun47631@gmail.com>
2024-11-26 11:56:03 +0900

    [delete/#45] : 사용하지 않는 에러 제거

--- src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
+++ src/main/java/fisa/woorizip/backend/bookmark/BookmarkErrorCode.java
@@ -1,20 +1,17 @@
 package fisa.woorizip.backend.bookmark;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import fisa.woorizip.backend.common.exception.errorcode.ErrorCode;
 
 import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 public enum BookmarkErrorCode implements ErrorCode {
     BOOKMARK_ALREADY_EXIST(CONFLICT, "이미 존재하는 북마크입니다."),
-    BOOKMARK_NOT_FOUND(NOT_FOUND, "북마크를 찾을 수 없습니다."),
-    BOOKMARK_MEMBER_NOT_FOUND(NOT_FOUND, "북마크의 회원을 찾을 수 없습니다."),
-    BOOKMARK_HOUSE_NOT_FOUND(NOT_FOUND, "북마크의 집을 찾을 수 없습니다.");
+    BOOKMARK_NOT_FOUND(NOT_FOUND, "북마크를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
