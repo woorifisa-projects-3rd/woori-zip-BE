ccbb76e3f80f75d0368f414175df70126bef79fa
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-26 11:36:25 +0900

    [feat/#33]: BookmarkListResponse 수정

--- src/main/java/fisa/woorizip/backend/bookmark/dto/response/BookmarkListResponse.java
+++ src/main/java/fisa/woorizip/backend/bookmark/dto/response/BookmarkListResponse.java
@@ -1,16 +1,14 @@
 package fisa.woorizip.backend.bookmark.dto.response;
 
 import fisa.woorizip.backend.bookmark.domain.Bookmark;
-
+import fisa.woorizip.backend.house.domain.House;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Builder
 public class BookmarkListResponse {
+
     private final Long bookmarkId;
     private final Long houseId;
     private final String houseName;
@@ -20,24 +18,19 @@ public class BookmarkListResponse {
     private final long deposit;
     private final long monthlyRentFee;
     private final String imageUrl;
-    private final LocalDateTime createdAt;
 
-    public static List<BookmarkListResponse> from(List<Bookmark> bookmarks) {
-        return bookmarks.stream()
-                .map(
-                        bookmark ->
-                                builder()
-                                        .bookmarkId(bookmark.getId())
-                                        .houseId(bookmark.getHouse().getId())
-                                        .houseName(bookmark.getHouse().getName())
-                                        .address(bookmark.getHouse().getAddress())
-                                        .gu(bookmark.getHouse().getGu())
-                                        .dong(bookmark.getHouse().getDong())
-                                        .deposit(bookmark.getHouse().getDeposit())
-                                        .monthlyRentFee(bookmark.getHouse().getMonthlyRentFee())
-                                        .imageUrl(bookmark.getHouse().getRepresentativeImage())
-                                        .createdAt(bookmark.getCreatedAt())
-                                        .build())
-                .toList();
+    public static BookmarkListResponse from(Bookmark bookmark) {
+        House house = bookmark.getHouse();
+        return BookmarkListResponse.builder()
+                .bookmarkId(bookmark.getId())
+                .houseId(house.getId())
+                .houseName(house.getName())
+                .address(house.getAddress())
+                .gu(house.getGu())
+                .dong(house.getDong())
+                .deposit(house.getDeposit())
+                .monthlyRentFee(house.getMonthlyRentFee())
+                .imageUrl(house.getRepresentativeImage())
+                .build();
     }
 }
