ab36645ff6625e30e549f47288e22e6b19002513
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-26 12:22:05 +0900

    [delete/#33]: load_csv 파일 제거

--- src/main/resources/db/migration/V2__load_csv.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE '/Users/jeongseogjin/backend/houses/woori-zip-BE/src/main/resources/data/consumption_analysis.csv'
-INTO TABLE consumption_analysis
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(customer_type, book, car, cloth, culture, food, grocery, customer_count);
