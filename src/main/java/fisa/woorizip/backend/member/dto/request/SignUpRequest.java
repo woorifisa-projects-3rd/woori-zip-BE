c44045edb7ab3e64d41d4260df9b734ab0987b13
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-26 12:43:52 +0900

    [feat/#30]: role에 따른 회원 가입 분리 추가

--- src/main/java/fisa/woorizip/backend/member/dto/request/SignUpRequest.java
+++ src/main/java/fisa/woorizip/backend/member/dto/request/SignUpRequest.java
@@ -1,75 +1,49 @@
 package fisa.woorizip.backend.member.dto.request;
 
-import fisa.woorizip.backend.member.domain.*;
+import fisa.woorizip.backend.member.domain.Member;
+import fisa.woorizip.backend.member.domain.Member.MemberBuilder;
+import fisa.woorizip.backend.member.domain.Role;
 
+import fisa.woorizip.backend.member.domain.Sex;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
+@SuperBuilder
+@AllArgsConstructor
 @Getter
 public class SignUpRequest {
 
     @NotBlank private String name;
-    @NotBlank private String username;
+    @Email private String username;
     @NotBlank private String password;
     @Past private LocalDate birthday;
-    @NotNull private Gender gender;
-    private Membership membership;
-    private LifeStage lifeStage;
-    @NotNull private Integer creditScore;
-    private long assets;
-    private long totalIncomeLastYear;
-    private Integer yearsOfMarriage;
-    private Integer monthsOfEmployment;
+    @NotNull private Sex sex;
+    private String licenseId;
 
     protected SignUpRequest() {}
 
-    public SignUpRequest(
-            String name,
-            String username,
-            String password,
-            LocalDate birthday,
-            Integer creditScore,
-            long assets,
-            long totalIncomeLastYear,
-            Integer yearsOfMarriage,
-            Integer monthsOfEmployment,
-            Gender gender,
-            Membership membership,
-            LifeStage lifeStage) {
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.birthday = birthday;
-        this.creditScore = creditScore;
-        this.assets = assets;
-        this.totalIncomeLastYear = totalIncomeLastYear;
-        this.yearsOfMarriage = yearsOfMarriage;
-        this.monthsOfEmployment = monthsOfEmployment;
-        this.gender = gender;
-        this.membership = membership;
-        this.lifeStage = lifeStage;
-    }
-
-    public Member toMember(String encodedPassword, Role role) {
+    public MemberBuilder getMemberBuilder(String encodedPassword) {
         return Member.builder()
                 .name(name)
                 .username(username)
                 .password(encodedPassword)
-                .birthday(birthday)
-                .creditScore(creditScore)
-                .role(role)
-                .assets(assets)
-                .totalIncomeLastYear(totalIncomeLastYear)
-                .yearsOfMarriage(yearsOfMarriage)
-                .monthsOfEmployment(monthsOfEmployment)
-                .gender(gender)
-                .membership(membership)
-                .lifeStage(lifeStage)
-                .build();
+                .birthday(birthday);
     }
+
+    public Member toAdminMember(String encodedPassword) {
+        return getMemberBuilder(encodedPassword).role(Role.ADMIN).build();
+    }
+
+    public Member toAgentMember(String encodedPassword) {
+        return getMemberBuilder(encodedPassword).licenseId(licenseId).role(Role.AGENT).build();
+    }
+
 }
--- src/main/java/fisa/woorizip/backend/member/service/MemberServiceImpl.java
+++ src/main/java/fisa/woorizip/backend/member/service/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package fisa.woorizip.backend.member.service;
 
+import static fisa.woorizip.backend.member.MemberErrorCode.AGENT_LICENSE_ID_IS_NULL;
+import static fisa.woorizip.backend.member.MemberErrorCode.AGENT_NOT_FOUND;
 import static fisa.woorizip.backend.member.MemberErrorCode.ALREADY_EXIST_USERNAME;
+import static fisa.woorizip.backend.member.MemberErrorCode.NOT_ALLOWED_SIGN_UP;
+import static java.util.Objects.isNull;
 
+import fisa.woorizip.backend.agent.AgentRepository;
 import fisa.woorizip.backend.common.exception.WooriZipException;
 import fisa.woorizip.backend.member.domain.Member;
 import fisa.woorizip.backend.member.domain.Role;
@@ -20,16 +25,38 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AgentRepository agentRepository;
 
     @Override
     @Transactional
     public void signUp(SignUpRequest signUpRequest, Role role) {
         String encodedPassword = passwordEncoder.encode(signUpRequest.getPassword());
-        if (role == Role.ADMIN) {
-        Member member =
-                signUpRequest.toMember(encodedPassword, role);
-        }
+        Member member = createMemberByRole(signUpRequest, role, encodedPassword);
         memberRepository.save(member);
+    }
+
+    private Member createMemberByRole(SignUpRequest signUpRequest, Role role, String encodedPassword) {
+        if (role == Role.ADMIN) {
+            return signUpRequest.toAdminMember(encodedPassword);
+        }
+        if (role == Role.AGENT) {
+            validateLicenseIdIsNull(signUpRequest);
+            validateExistAgent(signUpRequest);
+            return signUpRequest.toAgentMember(encodedPassword);
+        }
+        throw new WooriZipException(NOT_ALLOWED_SIGN_UP);
+    }
+
+    private void validateExistAgent(SignUpRequest signUpRequest) {
+        if(!agentRepository.existsByNameAndLicenseId(signUpRequest.getName(), signUpRequest.getLicenseId())){
+            throw new WooriZipException(AGENT_NOT_FOUND);
+        }
+    }
+
+    private void validateLicenseIdIsNull(SignUpRequest signUpRequest) {
+        if (isNull(signUpRequest.getLicenseId())) {
+            throw new WooriZipException(AGENT_LICENSE_ID_IS_NULL);
+        }
     }
 
     @Override
