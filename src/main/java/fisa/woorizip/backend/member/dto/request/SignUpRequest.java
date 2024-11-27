019496fa6e84c6b8ed0c322b02907c49071d1818
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-27 09:51:54 +0900

    [feat/#30]: member 생성시 status 추가

--- src/main/java/fisa/woorizip/backend/member/dto/request/SignUpRequest.java
+++ src/main/java/fisa/woorizip/backend/member/dto/request/SignUpRequest.java
@@ -1,31 +1,32 @@
 package fisa.woorizip.backend.member.dto.request;
 
+import fisa.woorizip.backend.member.domain.Gender;
 import fisa.woorizip.backend.member.domain.Member;
 import fisa.woorizip.backend.member.domain.Member.MemberBuilder;
 import fisa.woorizip.backend.member.domain.Role;
 
-import fisa.woorizip.backend.member.domain.Sex;
+import fisa.woorizip.backend.member.domain.Status;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
-@SuperBuilder
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
+@Builder
 public class SignUpRequest {
 
     @NotBlank private String name;
     @Email private String username;
     @NotBlank private String password;
     @Past private LocalDate birthday;
-    @NotNull private Sex sex;
+    @NotNull private Gender gender;
     private String licenseId;
 
     protected SignUpRequest() {}
@@ -35,15 +36,16 @@ public class SignUpRequest {
                 .name(name)
                 .username(username)
                 .password(encodedPassword)
-                .birthday(birthday);
+                .birthday(birthday)
+                .gender(gender);
     }
 
     public Member toAdminMember(String encodedPassword) {
-        return getMemberBuilder(encodedPassword).role(Role.ADMIN).build();
+        return getMemberBuilder(encodedPassword).role(Role.ADMIN).status(Status.PENDING_APPROVAL).build();
     }
 
     public Member toAgentMember(String encodedPassword) {
-        return getMemberBuilder(encodedPassword).licenseId(licenseId).role(Role.AGENT).build();
+        return getMemberBuilder(encodedPassword).licenseId(licenseId).role(Role.AGENT).status(Status.NOT_ADMIN).build();
     }
 
 }
