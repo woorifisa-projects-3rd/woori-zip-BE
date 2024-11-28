932e908597bfd98c340881c960dc1ae01867e6f0
qbobl5 <ddddd991125@gmail.com>
2024-11-28 17:24:42 +0900

    [feat/#52]: transactional 어노테이션 추가

--- src/main/java/fisa/woorizip/backend/recentlyloangoods/service/RecentlyLoanGoodsServiceImpl.java
+++ src/main/java/fisa/woorizip/backend/recentlyloangoods/service/RecentlyLoanGoodsServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
