7dbfaaa5a49c49238710cabb558672038d1a3b33
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-28 15:26:51 +0900

    [style/#53]: import문 수정

--- src/main/java/fisa/woorizip/backend/common/ApiResponseHandler.java
+++ src/main/java/fisa/woorizip/backend/common/ApiResponseHandler.java
@@ -5,6 +5,8 @@ import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -20,8 +22,7 @@ public class ApiResponseHandler implements ResponseBodyAdvice<Object> {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
                                   Class<? extends HttpMessageConverter<?>> selectedConverterType,
-                                  org.springframework.http.server.ServerHttpRequest request,
-                                  org.springframework.http.server.ServerHttpResponse response) {
+                                  ServerHttpRequest request, ServerHttpResponse response) {
 
         if (body instanceof ApiResponse) {
             return body;
