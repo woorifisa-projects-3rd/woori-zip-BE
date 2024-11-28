06c8839f06bc3c6c2a964ee57960aa059c06902e
qbobl5 <ddddd991125@gmail.com>
2024-11-28 11:40:23 +0900

    [feat/#52]: repository 추가

---
+++ src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
@@ -0,0 +1,6 @@
+package fisa.woorizip.backend.loangoods.repository;
+
+import fisa.woorizip.backend.loangoods.domain.LoanGoods;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanGoodsRepository extends JpaRepository<LoanGoods, Long> {}
