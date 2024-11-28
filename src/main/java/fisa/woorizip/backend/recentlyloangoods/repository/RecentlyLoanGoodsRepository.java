da997f1534a776d3dec1c882eb7cf4b3e029a42f
qbobl5 <ddddd991125@gmail.com>
2024-11-28 15:37:48 +0900

    [feat/#52]: 최근 본 대출상품 조회 query 추가

--- src/main/java/fisa/woorizip/backend/recentlyloangoods/repository/RecentlyLoanGoodsRepository.java
+++ src/main/java/fisa/woorizip/backend/recentlyloangoods/repository/RecentlyLoanGoodsRepository.java
@@ -1,7 +1,17 @@
 package fisa.woorizip.backend.recentlyloangoods.repository;
 
+import fisa.woorizip.backend.loangoods.domain.LoanGoods;
 import fisa.woorizip.backend.recentlyloangoods.domain.RecentlyLoanGoods;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface RecentlyLoanGoodsRepository extends JpaRepository<RecentlyLoanGoods, Long> {}
+public interface RecentlyLoanGoodsRepository extends JpaRepository<RecentlyLoanGoods, Long> {
+
+    @Query(
+            "SELECT rg.loanGoods FROM RecentlyLoanGoods rg WHERE rg.member.id = :memberId ORDER BY rg.lookedAt DESC")
+    Slice<LoanGoods> findLoanGoodsByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+}
