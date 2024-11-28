6d558efef51aed3cc14e09be37d4f6c56efca2d9
qbobl5 <ddddd991125@gmail.com>
2024-11-28 15:37:57 +0900

    [feat/#52]: 최근 본 대출상품 조회 서비스 로직 구현

--- src/main/java/fisa/woorizip/backend/recentlyloangoods/service/RecentlyLoanGoodsService.java
+++ src/main/java/fisa/woorizip/backend/recentlyloangoods/service/RecentlyLoanGoodsService.java
@@ -1,3 +1,10 @@
 package fisa.woorizip.backend.recentlyloangoods.service;
 
-public interface RecentlyLoanGoodsService {}
+import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
+import fisa.woorizip.backend.recentlyloangoods.dto.ShowRecentlyLoanGoodsResponse;
+import org.springframework.data.domain.Pageable;
+
+public interface RecentlyLoanGoodsService {
+
+    ShowRecentlyLoanGoodsResponse getRecentlyLoanGoods(MemberIdentity memberIdentity, Pageable pageable);
+}
