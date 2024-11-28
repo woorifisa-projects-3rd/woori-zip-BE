5f72f4b724639264214a30cabec5df71cbe2e4bc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-28 17:49:38 +0900

    [feat/#57]: 권한 취소 Controller 추가
    - 성공시 200 OK

--- src/main/java/fisa/woorizip/backend/member/controller/MemberController.java
+++ src/main/java/fisa/woorizip/backend/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package fisa.woorizip.backend.member.controller;
 
+import fisa.woorizip.backend.member.controller.auth.Login;
 import fisa.woorizip.backend.member.domain.Role;
+import fisa.woorizip.backend.member.dto.request.RevokeApprovalRequest;
 import fisa.woorizip.backend.member.dto.request.SignUpRequest;
 import fisa.woorizip.backend.member.service.MemberService;
 
@@ -9,6 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+
+import static fisa.woorizip.backend.member.domain.Role.ADMIN;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -35,6 +40,13 @@ public class MemberController {
     @GetMapping("/members/valid")
     public ResponseEntity<Void> validateUsername(@RequestParam(name = "username") String username) {
         memberService.validateAlreadyExistUsername(username);
+        return ResponseEntity.ok().build();
+    }
+
+    @Login(role = ADMIN)
+    @DeleteMapping("/admins")
+    public ResponseEntity<Void> revokeApproval(@RequestBody @Valid RevokeApprovalRequest revokeApprovalRequest) {
+        memberService.revokeApprovals(revokeApprovalRequest);
         return ResponseEntity.ok().build();
     }
 }
