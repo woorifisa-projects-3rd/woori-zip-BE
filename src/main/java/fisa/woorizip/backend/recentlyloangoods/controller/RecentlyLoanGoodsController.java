698bc5a8c48bd4dfab94c5e04e98a494ad8eb332
qbobl5 <ddddd991125@gmail.com>
2024-11-28 15:38:08 +0900

    [feat/#52]: 최근 본 대출상품 조회 컨트롤러 구현

--- src/main/java/fisa/woorizip/backend/recentlyloangoods/controller/RecentlyLoanGoodsController.java
+++ src/main/java/fisa/woorizip/backend/recentlyloangoods/controller/RecentlyLoanGoodsController.java
@@ -1,15 +1,28 @@
 package fisa.woorizip.backend.recentlyloangoods.controller;
 
+import fisa.woorizip.backend.member.controller.auth.MemberIdentity;
+import fisa.woorizip.backend.member.controller.auth.VerifiedMember;
+import fisa.woorizip.backend.recentlyloangoods.dto.ShowRecentlyLoanGoodsResponse;
 import fisa.woorizip.backend.recentlyloangoods.service.RecentlyLoanGoodsService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/loan/recently")
 @RequiredArgsConstructor
 public class RecentlyLoanGoodsController {
     private final RecentlyLoanGoodsService recentlyLoanGoodsService;
+
+    @GetMapping
+    public ShowRecentlyLoanGoodsResponse showRecentlyLoanGoods(
+            @VerifiedMember MemberIdentity memberIdentity,
+            @PageableDefault(size = 5) Pageable pageable) {
+        return recentlyLoanGoodsService.getRecentlyLoanGoods(memberIdentity, pageable);
+    }
 }
