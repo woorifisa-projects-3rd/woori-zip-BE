b9f53863616078cefaef2195b016d23819f9eacd
qbobl5 <ddddd991125@gmail.com>
2024-11-28 11:38:46 +0900

    [feat/#52]: 대출 금리 추가

---
+++ src/main/java/fisa/woorizip/backend/rate/domain/Rate.java
@@ -0,0 +1,39 @@
+package fisa.woorizip.backend.rate.domain;
+
+import fisa.woorizip.backend.loangoods.domain.LoanGoods;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "loan_goods_id", nullable = false)
+    private LoanGoods loanGoods;
+
+    @Column(name = "rate_type", nullable = false)
+    private RateType rateType;
+
+    @Column(name = "basic", nullable = false)
+    private String basic;
+
+    @Column(name = "add", nullable = false)
+    private String add;
+
+    @Column(name = "normal", nullable = false)
+    private String normal;
+
+    @Column(name = "special", nullable = false)
+    private String special;
+
+    @Column(name = "min", nullable = false)
+    private String min;
+}
