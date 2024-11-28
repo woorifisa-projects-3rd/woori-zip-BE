f5f0500e3a2d61af202980ac6e112416ea2ec660
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-28 14:46:08 +0900

    [feat/#53]: API Response로 감싸는 Handler 추가

---
+++ src/main/java/fisa/woorizip/backend/common/ApiResponseHandler.java
@@ -0,0 +1,36 @@
+package fisa.woorizip.backend.common;
+
+import fisa.woorizip.backend.common.exception.response.ErrorResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+@Order(2)
+public class ApiResponseHandler implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  org.springframework.http.server.ServerHttpRequest request,
+                                  org.springframework.http.server.ServerHttpResponse response) {
+
+        if (body instanceof ApiResponse) {
+            return body;
+        }
+
+        if (body instanceof ErrorResponse) {
+            return ApiResponse.fail(body);
+        }
+
+        return ApiResponse.success(body);
+    }
+}
