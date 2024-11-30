df672b0988e9f52eaaafa19b5527997f04db7898
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-30 18:10:08 +0900

    [test/#63]: Role과 Pageable로 회원 조회 테스트

--- src/test/java/fisa/woorizip/backend/member/repository/MemberRepositoryTest.java
+++ src/test/java/fisa/woorizip/backend/member/repository/MemberRepositoryTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import fisa.woorizip.backend.member.domain.Member;
+import fisa.woorizip.backend.member.domain.Role;
 import fisa.woorizip.backend.support.RepositoryTest;
 import fisa.woorizip.backend.support.fixture.MemberFixture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.Optional;
 
@@ -28,5 +31,20 @@ class MemberRepositoryTest {
         assertAll(
                 () -> assertThat(findMember).isPresent(),
                 () -> assertThat(findMember.get().getUsername()).isEqualTo(member.getUsername()));
+    }
+
+    @Test
+    void role과_Pageable로_회원들을_조회할_수_있댜() {
+        Member member1 = 저장(MemberFixture.builder().role(Role.ADMIN).build());
+        Member member2 = 저장(MemberFixture.builder().role(Role.MEMBER).build());
+        Member member3 = 저장(MemberFixture.builder().role(Role.ADMIN).build());
+
+
+        Page<Member> members = memberRepository.findAllByRole(Role.ADMIN, PageRequest.of(0, 2));
+        assertAll(
+                () -> assertThat(members.getNumber()).isEqualTo(0),
+                () -> assertThat(members.getTotalPages()).isEqualTo(1),
+                () -> assertThat(members.getContent()).containsExactly(member1, member3)
+        );
     }
 }
