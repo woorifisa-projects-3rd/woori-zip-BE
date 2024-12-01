cb7fcc6307d68269b93ac99b6d7fdc956fe2e880
qbobl5 <ddddd991125@gmail.com>
2024-12-01 12:57:27 +0900

    [feat/#65]: findAllBy 메소드 추가

--- src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
+++ src/main/java/fisa/woorizip/backend/loangoods/repository/LoanGoodsRepository.java
@@ -2,6 +2,11 @@ package fisa.woorizip.backend.loangoods.repository;
 
 import fisa.woorizip.backend.loangoods.domain.LoanGoods;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LoanGoodsRepository extends JpaRepository<LoanGoods, Long> {}
+public interface LoanGoodsRepository extends JpaRepository<LoanGoods, Long> {
+
+    Slice<LoanGoods> findAllBy(Pageable pageable);
+}
