f793b967fc266faf390f33b107984009418b9b1f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-12-01 23:08:36 +0900

    [feat/#67]: 쿼리파라미터의 null 허용, LocalDateTime의 format 지정

--- src/main/java/fisa/woorizip/backend/log/controller/LogController.java
+++ src/main/java/fisa/woorizip/backend/log/controller/LogController.java
@@ -7,6 +7,7 @@ import fisa.woorizip.backend.member.controller.auth.Login;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 import static fisa.woorizip.backend.member.domain.Role.ADMIN;
+import static java.util.Objects.isNull;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,9 +27,9 @@ public class LogController {
 
     @Login(role = ADMIN)
     @GetMapping("/logs")
-    public ShowLogsResponse showLogs(@RequestParam("username") String username,
-                                     @RequestParam("startDate") LocalDateTime startDate,
-                                     @RequestParam("endDate") LocalDateTime endDate,
+    public ShowLogsResponse showLogs(@RequestParam(value = "username", required = false) String username,
+                                     @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime startDate,
+                                     @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime endDate,
                                      Pageable pageable) {
         return logService.searchLogs(username, startDate, endDate, pageable);
     }
