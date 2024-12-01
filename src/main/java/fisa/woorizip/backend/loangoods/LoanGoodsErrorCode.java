ffe22b2bcf8854bd947dc997a3b2e85b792a3687
qbobl5 <ddddd991125@gmail.com>
2024-12-01 13:08:14 +0900

    [feat/#65]: 대출 상품이 존재하지 않는 경우 예외 코드 추가

---
+++ src/main/java/fisa/woorizip/backend/loangoods/LoanGoodsErrorCode.java
@@ -0,0 +1,20 @@
+package fisa.woorizip.backend.loangoods;
+
+import fisa.woorizip.backend.common.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
+public enum LoanGoodsErrorCode implements ErrorCode {
+    LOAN_GOODS_NOT_FOUND(NOT_FOUND, "대출 상품이 존재하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    LoanGoodsErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
