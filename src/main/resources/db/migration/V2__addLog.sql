ed7323f8cd40cd7615525f035b40450c69966c0b
rlfrkdms1 <rlfrkdms1@naver.com>
2024-12-03 17:20:20 +0900

    [feat/#74]: log에 member_id null로 변경

--- src/main/resources/db/migration/V2__addLog.sql
+++ src/main/resources/db/migration/V2__addLog.sql
@@ -3,3 +3,4 @@ alter table `log` add column `request_url` VARCHAR(255) NOT NULL;
 alter table `log` add column `request_body` TEXT NOT NULL;
 alter table `log` add column `response_status` VARCHAR(255) NOT NULL;
 alter table `log` add column `response_body` TEXT NOT NULL;
+alter table `log` modify member_id BIGINT NULL;
