2dfa895f6b847fa211f8734932b55dc6c7d05540
qbobl5 <ddddd991125@gmail.com>
2024-12-04 16:00:19 +0900

    [feat/#73]: 대출 상품 추천 시 필요한 대출 체크리스트 필터 생성

---
+++ src/main/java/fisa/woorizip/backend/loanchecklist/dto/request/LoanGoodsCheckListFilter.java
@@ -0,0 +1,38 @@
+package fisa.woorizip.backend.loanchecklist.dto.request;
+
+import fisa.woorizip.backend.house.domain.House;
+
+import fisa.woorizip.backend.loanchecklist.domain.MarriageStatus;
+import fisa.woorizip.backend.loanchecklist.domain.WorkStatus;
+import fisa.woorizip.backend.loanchecklist.domain.WorkTerm;
+import fisa.woorizip.backend.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LoanGoodsCheckListFilter {
+    private WorkStatus workStatus;
+    private WorkTerm workTerm;
+    private Long annualIncome;
+    private Long totalAssets;
+    private Boolean contract;
+    private MarriageStatus marriageStatus;
+    private long leaseDeposit;
+    private Long monthlyRentFee;
+    private double exclusiveArea;
+
+    public static LoanGoodsCheckListFilter of(House house, LoanGoodsCheckListRequest request) {
+        return builder()
+                .workStatus(request.getWorkStatus())
+                .workTerm(request.getWorkTerm())
+                .annualIncome(request.getAnnualIncome())
+                .totalAssets(request.getTotalAssets())
+                .contract(request.getContract())
+                .marriageStatus(request.getMarriageStatus())
+                .leaseDeposit(house.getDeposit())
+                .monthlyRentFee(house.getMonthlyRentFee())
+                .exclusiveArea(house.getExclusiveArea())
+                .build();
+    }
+}
