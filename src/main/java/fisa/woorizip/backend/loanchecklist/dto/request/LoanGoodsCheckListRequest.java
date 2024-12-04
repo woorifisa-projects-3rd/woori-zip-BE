840b0af59518421c07d1a41a4f870a9b888d99e7
qbobl5 <ddddd991125@gmail.com>
2024-12-04 15:55:37 +0900

    [feat/#73]: 체크리스트 요청 시 필요한 변수를 Request 파일로 생성 (파라미터)

---
+++ src/main/java/fisa/woorizip/backend/loanchecklist/dto/request/LoanGoodsCheckListRequest.java
@@ -0,0 +1,45 @@
+package fisa.woorizip.backend.loanchecklist.dto.request;
+
+import fisa.woorizip.backend.loanchecklist.domain.MarriageStatus;
+import fisa.woorizip.backend.loanchecklist.domain.WorkStatus;
+import fisa.woorizip.backend.loanchecklist.domain.WorkTerm;
+
+import lombok.*;
+
+import java.beans.ConstructorProperties;
+
+@Getter
+@Builder
+public class LoanGoodsCheckListRequest {
+
+    private Long annualIncome;
+    private Long totalAssets;
+    private MarriageStatus marriageStatus;
+    private boolean contract;
+    private WorkStatus workStatus;
+    private WorkTerm workTerm;
+
+    @ConstructorProperties({
+            "annualIncome",
+            "totalAssets",
+            "marriageStatus",
+            "contract",
+            "workStatus",
+            "workTerm"
+    })
+    private LoanGoodsCheckListRequest(
+            Long annualIncome,
+            Long totalAssets,
+            String marriageStatus,
+            boolean contract,
+            String workStatus,
+            String workTerm
+    ) {
+        this.annualIncome = annualIncome;
+        this.totalAssets = totalAssets;
+        this.marriageStatus = MarriageStatus.from(marriageStatus);
+        this.contract = contract;
+        this.workStatus = WorkStatus.from(workStatus);
+        this.workTerm = WorkTerm.from(workTerm);
+    }
+}
