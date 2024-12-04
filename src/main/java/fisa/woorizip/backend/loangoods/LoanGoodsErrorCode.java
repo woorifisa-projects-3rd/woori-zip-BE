0dc10c1ac2cf652b38cfcc1bd1c4a0ed311aac1e
qbobl5 <ddddd991125@gmail.com>
2024-12-04 17:58:59 +0900

    [feat/#73]: 대출 상품 타입이 존재하지 않는 경우 예외 코드 생성

--- src/main/java/fisa/woorizip/backend/loangoods/LoanGoodsErrorCode.java
+++ src/main/java/fisa/woorizip/backend/loangoods/LoanGoodsErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum LoanGoodsErrorCode implements ErrorCode {
-    LOAN_GOODS_NOT_FOUND(NOT_FOUND, "대출 상품이 존재하지 않습니다.");
+    LOAN_GOODS_NOT_FOUND(NOT_FOUND, "대출 상품이 존재하지 않습니다."),
+    LOAN_TYPE_NOT_FOUND(NOT_FOUND, "존재하지 않는 대출 상품 타입입니다.");
 
     private final HttpStatus status;
     private final String message;
