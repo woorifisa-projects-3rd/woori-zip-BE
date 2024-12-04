cf59b77ecb338f3238e03a993c00dc81b9cba199
qbobl5 <ddddd991125@gmail.com>
2024-12-04 15:54:32 +0900

    [fix/#73]: enum 값 추가 및 수정

--- src/main/java/fisa/woorizip/backend/loanchecklist/domain/WorkStatus.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/domain/WorkStatus.java
@@ -2,15 +2,24 @@ package fisa.woorizip.backend.loanchecklist.domain;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum WorkStatus {
     UNEMPLOYED("비작징인"),
     EMPLOYED("직장인"),
-    ALL("전체");
+    NONE_WORK_STATUS("해당없음");
 
     private final String name;
 
     WorkStatus(String name) {
         this.name = name;
+    }
+
+    public static WorkStatus from(String name) {
+        return Arrays.stream(WorkStatus.values())
+                .filter(workStatus -> workStatus.name.equals(name))
+                .findAny()
+                .orElse(NONE_WORK_STATUS);
     }
 }
