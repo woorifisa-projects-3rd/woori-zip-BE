b6fb7fe6c63d6f88156d3ad9f663e0a18316f23c
qbobl5 <ddddd991125@gmail.com>
2024-12-04 16:54:49 +0900

    [feat/#73]: 필요없는 컬럼 제거하는 sql문 추가

--- src/main/resources/db/migration/V2__modify.sql
+++ src/main/resources/db/migration/V2__modify.sql
@@ -5,6 +5,7 @@ ALTER TABLE `member`
 DROP COLUMN `available_assets`;
 
 ALTER TABLE `loan_checklist`
+    DROP COLUMN `available_assets`,
     MODIFY `annual_income` BIGINT NULL,
     MODIFY `total_assets` BIGINT NULL,
     MODIFY `lease_deposit` BIGINT NULL,
