80bced2054efb6f17b4c5d2369d3813cc93bfc5c
qbobl5 <ddddd991125@gmail.com>
2024-12-04 16:38:53 +0900

    [feat/#73]: 대출 추천을 위한 querydsl 메소드 추가

--- src/main/java/fisa/woorizip/backend/loanchecklist/repository/LoanCheckListCustomRepository.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/repository/LoanCheckListCustomRepository.java
@@ -1,16 +1,12 @@
 package fisa.woorizip.backend.loanchecklist.repository;
 
-import fisa.woorizip.backend.loanchecklist.dto.request.RecommendLoanFilter;
-import fisa.woorizip.backend.loanchecklist.dto.request.RecommendMemberInfoFilter;
+import fisa.woorizip.backend.loanchecklist.dto.LoanCheckListFilter;
+import fisa.woorizip.backend.loanchecklist.dto.request.LoanCheckListRequest;
 import fisa.woorizip.backend.loangoods.domain.LoanGoods;
 
 import java.util.List;
 
 public interface LoanCheckListCustomRepository {
 
-    List<LoanGoods> findRecommendLoanGoodsFirstTime(RecommendLoanFilter recommendLoanFilter);
-
-    List<LoanGoods> findRecommendLoanGoods(
-            RecommendLoanFilter recommendLoanFilter,
-            RecommendMemberInfoFilter recommendMemberInfoFilter);
+    List<LoanGoods> findRecommendLoanGoods(LoanCheckListFilter filter);
 }
