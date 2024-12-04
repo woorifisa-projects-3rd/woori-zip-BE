8d0fe57ad7280be7fc07c094c7aee788cc62e2eb
qbobl5 <ddddd991125@gmail.com>
2024-12-04 16:02:01 +0900

    [refactor/#73]: 위치 리팩토링

--- src/main/java/fisa/woorizip/backend/loanchecklist/dto/LoanGoodsCheckListFilter.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/dto/LoanGoodsCheckListFilter.java
@@ -1,11 +1,11 @@
-package fisa.woorizip.backend.loanchecklist.dto.request;
+package fisa.woorizip.backend.loanchecklist.dto;
 
 import fisa.woorizip.backend.house.domain.House;
 
 import fisa.woorizip.backend.loanchecklist.domain.MarriageStatus;
 import fisa.woorizip.backend.loanchecklist.domain.WorkStatus;
 import fisa.woorizip.backend.loanchecklist.domain.WorkTerm;
-import fisa.woorizip.backend.member.domain.Member;
+import fisa.woorizip.backend.loanchecklist.dto.request.LoanGoodsCheckListRequest;
 import lombok.Builder;
 import lombok.Getter;
 
