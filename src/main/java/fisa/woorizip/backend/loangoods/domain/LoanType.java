742a487ff303176393a91906ea5c3899a465a5e1
qbobl5 <ddddd991125@gmail.com>
2024-12-04 17:59:17 +0900

    [feat/#73]: 대출 상품 타입 생성하는 메소드 추가

--- src/main/java/fisa/woorizip/backend/loangoods/domain/LoanType.java
+++ src/main/java/fisa/woorizip/backend/loangoods/domain/LoanType.java
@@ -1,6 +1,11 @@
 package fisa.woorizip.backend.loangoods.domain;
 
+import fisa.woorizip.backend.common.exception.WooriZipException;
 import lombok.Getter;
+
+import java.util.Arrays;
+
+import static fisa.woorizip.backend.loangoods.LoanGoodsErrorCode.LOAN_TYPE_NOT_FOUND;
 
 @Getter
 public enum LoanType {
@@ -11,5 +16,12 @@ public enum LoanType {
 
     LoanType(String name) {
         this.name = name;
+    }
+
+    public static LoanType from(String name) {
+        return Arrays.stream(LoanType.values())
+                .filter(loanType -> loanType.name.equals(name))
+                .findAny()
+                .orElseThrow(() -> new WooriZipException(LOAN_TYPE_NOT_FOUND));
     }
 }
