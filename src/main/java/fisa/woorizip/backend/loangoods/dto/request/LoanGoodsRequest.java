f61211f0e5441acc03219e50b3c11ae9b50fd8b7
qbobl5 <ddddd991125@gmail.com>
2024-12-04 17:58:43 +0900

    [feat/#73]: 대출 상품 수정 시 보내는 request dto 생성

---
+++ src/main/java/fisa/woorizip/backend/loangoods/dto/request/LoanGoodsRequest.java
@@ -0,0 +1,54 @@
+package fisa.woorizip.backend.loangoods.dto.request;
+
+import fisa.woorizip.backend.loangoods.domain.LoanType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.ToString;
+
+import static java.util.Objects.isNull;
+
+@Getter
+@ToString
+public class LoanGoodsRequest {
+    @NotBlank private String name;
+    @NotBlank private LoanType loanType;
+    @NotBlank private String target;
+    @NotBlank private String limitAmount;
+    @NotBlank private String term;
+    @NotBlank private String normalRate;
+    @NotBlank private String specialRate;
+    @NotBlank private String repayType;
+    @NotBlank private String guarantee;
+    @NotBlank private String targetHouse;
+    @NotBlank private String customerCost;
+    @NotBlank private String interestMethod;
+
+    public LoanGoodsRequest(
+            String name,
+            String loanType,
+            String target,
+            String limitAmount,
+            String term,
+            String normalRate,
+            String specialRate,
+            String repayType,
+            String guarantee,
+            String targetHouse,
+            String customerCost,
+            String interestMethod) {
+        this.name = name;
+        this.loanType = LoanType.from(loanType);
+        this.target = target;
+        this.limitAmount = limitAmount;
+        this.term = term;
+        this.normalRate = normalRate;
+        this.specialRate = specialRate;
+        this.repayType = repayType;
+        this.guarantee = guarantee;
+        this.targetHouse = targetHouse;
+        this.customerCost = customerCost;
+        this.interestMethod = interestMethod;
+    }
+
+    protected LoanGoodsRequest() {}
+}
