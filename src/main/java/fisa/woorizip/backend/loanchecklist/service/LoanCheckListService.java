b45a70deace8218ad5aba494ca076f37e504b763
qbobl5 <ddddd991125@gmail.com>
2024-12-04 16:40:25 +0900

    [feat/#73]: 대출 추천을 위한 서비스 로직 추가

--- src/main/java/fisa/woorizip/backend/loanchecklist/service/LoanCheckListService.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/service/LoanCheckListService.java
@@ -1,14 +1,12 @@
 package fisa.woorizip.backend.loanchecklist.service;
 
-import fisa.woorizip.backend.loanchecklist.dto.request.RecommendMemberInfoFilter;
+import fisa.woorizip.backend.loanchecklist.dto.request.LoanCheckListRequest;
 import fisa.woorizip.backend.loangoods.dto.response.LoanGoodsResponse;
 
 import java.util.List;
 
 public interface LoanCheckListService {
 
-    List<LoanGoodsResponse> getLoanGoodsRecommendByMemberIdAndHouseId(Long houseId);
-
-    List<LoanGoodsResponse> getLoanGoodsRecommend(
-            RecommendMemberInfoFilter recommendMemberInfoFilter, Long houseId);
+    List<LoanGoodsResponse> getRecommendLoanGoods(
+            Long houseId, LoanCheckListRequest loanGoodsCheckListRequest);
 }
