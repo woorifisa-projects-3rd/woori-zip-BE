a38ffab1d82fd8474c45f792be8f72aeaeda480b
qbobl5 <ddddd991125@gmail.com>
2024-12-05 15:02:40 +0900

    [feat/#78]: 회원 정보 수정하는 request dto 추가

---
+++ src/main/java/fisa/woorizip/backend/member/dto/request/MemberProfileRequest.java
@@ -0,0 +1,20 @@
+package fisa.woorizip.backend.member.dto.request;
+
+import fisa.woorizip.backend.member.domain.Gender;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class MemberProfileRequest {
+    @Past LocalDate birthday;
+    @NotNull private Gender gender;
+
+    protected MemberProfileRequest() {}
+}
