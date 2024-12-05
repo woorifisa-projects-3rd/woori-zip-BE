8909debeb5e82218477e4f68b89d5dcb3e7022ab
qbobl5 <ddddd991125@gmail.com>
2024-12-05 21:33:36 +0900

    [feat/#77]: Rate 와 관련된 예외 코드 파일 추가 및 금리 정보가 존재하지 않는 경우 예외 코드 추가

---
+++ src/main/java/fisa/woorizip/backend/rate/RateErrorCode.java
@@ -0,0 +1,20 @@
+package fisa.woorizip.backend.rate;
+
+import fisa.woorizip.backend.common.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
+public enum RateErrorCode implements ErrorCode {
+    RATE_NOT_FOUND(NOT_FOUND, "금리 정보가 존재하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    RateErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
