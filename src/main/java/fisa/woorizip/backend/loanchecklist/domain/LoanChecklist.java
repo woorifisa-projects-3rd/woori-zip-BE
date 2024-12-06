9d4275c3e4bafd6443cb005ec320db9684ebc979
qbobl5 <ddddd991125@gmail.com>
2024-12-06 15:34:50 +0900

    [fix/#85]: 대출 추천 시 계약 여부를 null 허용으로 수정하고 이에 대한 예외 처리

--- src/main/java/fisa/woorizip/backend/loanchecklist/domain/LoanChecklist.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/domain/LoanChecklist.java
@@ -35,8 +35,8 @@ public class LoanChecklist {
     @Column(name = "total_assets")
     private Long totalAssets;
 
-    @Column(name = "contract", nullable = false)
-    private boolean contract;
+    @Column(name = "contract")
+    private Boolean contract;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "marriage_status", nullable = false)
