6d085dedbc65247754a2dbdf84f2c24f326c59e3
qbobl5 <ddddd991125@gmail.com>
2024-12-06 16:12:00 +0900

    [feat/#85]: 대출 체크리스트 조회에 사용되는 에러 코드 추가

--- src/main/java/fisa/woorizip/backend/loanchecklist/LoanChecklistErrorCode.java
+++ src/main/java/fisa/woorizip/backend/loanchecklist/LoanChecklistErrorCode.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @Getter
 public enum LoanChecklistErrorCode implements ErrorCode {
-    ;
+    LOAN_CHECKLIST_NOT_FOUND(NOT_FOUND, "대출 상품에 해당하는 체크리스트가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
