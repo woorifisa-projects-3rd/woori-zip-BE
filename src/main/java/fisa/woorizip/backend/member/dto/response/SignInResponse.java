80f611bc0f12722fd885e995872d690a772fc417
rlfrkdms1 <rlfrkdms1@naver.com>
2024-12-06 09:20:48 +0900

    [feat]: 로그인 response에 role 추가

--- src/main/java/fisa/woorizip/backend/member/dto/response/SignInResponse.java
+++ src/main/java/fisa/woorizip/backend/member/dto/response/SignInResponse.java
@@ -2,6 +2,7 @@ package fisa.woorizip.backend.member.dto.response;
 
 import fisa.woorizip.backend.member.domain.Member;
 
+import fisa.woorizip.backend.member.domain.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -12,18 +13,17 @@ public class SignInResponse {
 
     private String accessToken;
     private String name;
+    private Role role;
 
     @Builder
-    private SignInResponse(String accessToken, String name) {
+    private SignInResponse(String accessToken, String name, Role role) {
         this.accessToken = accessToken;
         this.name = name;
+        this.role = role;
     }
 
     public static SignInResponse of(String accessToken, Member member) {
-        return SignInResponse.builder().accessToken(accessToken).name(member.getName()).build();
+        return SignInResponse.builder().accessToken(accessToken).name(member.getName()).role(member.getRole()).build();
     }
 
-    public static SignInResponse of(String accessToken, String name) {
-        return SignInResponse.builder().accessToken(accessToken).name(name).build();
-    }
 }
